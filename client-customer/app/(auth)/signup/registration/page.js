7f8fe0823f623b9257733efab9dca3f600ae0679
Lê Trần Thái Tâm <letranthaitam3000@gmail.com>
2024-01-06 23:47:56 +0700

    Update code formatting and fix bug in login functionality

--- client-customer/app/(auth)/signup/registration/page.js
+++ client-customer/app/(auth)/signup/registration/page.js
@@ -1,7 +1,21 @@
+'use client'
+import { useState, useEffect } from 'react';
+
 export default function RegisterForm() {
+  const [userEmail, setUserEmail] = useState('');
+
+  useEffect(() => {
+    const email = localStorage.getItem('userEmail');
+    setUserEmail(email);
+  }, []);
+
   return (
     <div>
       <h1>Register Form</h1>
+      <input 
+        type="text"
+        value={userEmail}
+      />
     </div>
   )
 }
--- client-customer/app/ui/signup/SigupForm.jsx
+++ client-customer/app/ui/signup/SigupForm.jsx
@@ -1,10 +1,68 @@
+import { useState, useEffect } from 'react';
 import { RedButton } from "@/app/ui/RedButton";
 import { IoIosArrowForward } from "react-icons/io";
+import { useRouter } from 'next/navigation';
 
 export function SignupForm() {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleEmailChange = (event) => {
+    let inputEmail = event.target.value.trimStart();
+    setEmail(inputEmail);
+    // Kiểm tra xem email có rỗng không
+    if (inputEmail === '') {
+      setErrorMessage('Bạn cần nhập vào email.');
+    } else if (inputEmail.length < 5) {
+      setErrorMessage(''); // Không hiển thị lỗi nếu độ dài email ít hơn 5
+    } else {
+      // Kiểm tra xem email có hợp lệ không
+      const emailRegex = /^[^\s@]{5,}@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(inputEmail)) {
+        setErrorMessage('Email không hợp lệ.');
+      } else {
+        setErrorMessage('');
+      }
+    }
+  };
   
+  // const handleFocus = () => {
+  //   setIsFocused(true);
+  // };
+  
+  // const handleBlur = () => {
+  //   setIsFocused(false);
+  // };
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Kiểm tra xem email có hợp lệ không
+    if (errorMessage) {
+      return;
+    }
+    // Lưu email vào localStorage để sử dụng ở trang kế tiếp
+    localStorage.setItem('userEmail', email);
+    // Chuyển hướng đến trang /signup/registration
+    router.push('/signup/registration');
+  };
+
+  const existingEmail = () => {
+    const emailExist = localStorage.getItem('userEmail');
+    if (emailExist) {
+      setEmail(emailExist);
+    } else {
+      setEmail('');
+    }
+  }
+  
+  useEffect(() => {
+    existingEmail();
+  }, []);
+
   return (
-    <form className="sub-form">
+    <form className="sub-form" onSubmit={handleSubmit}>
       <h3 className="sub-header">
         Bạn đã sẵn sàng xem chưa? Nhập email để tạo hoặc kích hoạt
         lại tư cách thành viên của bạn.
@@ -16,6 +74,11 @@ export function SignupForm() {
             type="text"
             className="text-input font-normal border-gray-600 bg-black/45 border rounded"
             required
+            value={email}
+            onChange={handleEmailChange}
+            // onFocus={handleFocus}
+            // onBlur={handleBlur}
+            aria-describedby="emailError"
           />
           <label
             htmlFor="email"
@@ -23,8 +86,9 @@ export function SignupForm() {
           >
             Địa chỉ email
           </label>
+          {errorMessage && !isFocused && <span id="emailError">{errorMessage}</span>}
         </div>
-        <RedButton className="w-[162px] h-14 text-[25px] flex items-center justify-center">
+        <RedButton type='submit' className="w-[162px] h-14 text-[25px] flex items-center justify-center">
           Bắt đầu <IoIosArrowForward />
         </RedButton>
       </div>
