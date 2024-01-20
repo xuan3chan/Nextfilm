cb3f9b4b58767742dc7b1c280c996af5d4e1679f
Lê Trần Thái Tâm <letranthaitam3000@gmail.com>
2024-01-20 15:27:21 +0700

    Refactor login and browse components

--- client-customer/app/browse/layout.js
+++ client-customer/app/browse/layout.js
@@ -4,7 +4,9 @@ export default function BrowseLayout({children}) {
   return (
     <>
       <Header />
-      {children}
+      <div className="h-screen">
+        {children}
+      </div>
       <Footer />
     </>
   )
