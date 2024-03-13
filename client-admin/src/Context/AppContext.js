a8da6c765df2bc01d46a6c1727b985faba03ffba
Thieen <thienctv03@gmail.com>
2024-03-13 16:27:53 +0700

    hi

---
+++ client-admin/src/Context/AppContext.js
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+export const AppContext = createContext({});
+
+export const AppProvider = ({ children }) => {
+  return (
+    <AppContext.Provider value={{ categoryList, token }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
