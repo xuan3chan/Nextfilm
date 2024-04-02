6968e683c4f7a3204983ab71ec0ad5294a27f965
xuandd <103272537+xuan3chan@users.noreply.github.com>
2024-04-03 01:27:14 +0700

    asdkm

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const Express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const fs = require("fs");
 //import routes
 const authUsersRoute = require("./Routes/authUsersRoute");
 const authAdminRoute = require("./Routes/authAdminRoute");
